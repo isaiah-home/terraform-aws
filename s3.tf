5d7317a08ab71ebe1af9365829b33c4f1fae53e4
isaiah-v <isaiah.v@comcast.net>
2022-05-24 10:20:01 -0700

    added a bucket for nextcloud to use as primary storage

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
+# Backup data
 resource "aws_s3_bucket" "backup" {
    bucket = "backups.${var.domain}"
 }
-
 resource "aws_s3_bucket_public_access_block" "backup" {
   bucket = aws_s3_bucket.backup.id
 
@@ -10,14 +10,12 @@ resource "aws_s3_bucket_public_access_block" "backup" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
 resource "aws_s3_bucket_versioning" "backup" {
   bucket = aws_s3_bucket.backup.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-
 resource "aws_s3_bucket_lifecycle_configuration" "backup_experation" {
   bucket = aws_s3_bucket.backup.id
 
@@ -29,4 +27,17 @@ resource "aws_s3_bucket_lifecycle_configuration" "backup_experation" {
       newer_noncurrent_versions = 5
     }
   }
+}
+
+#  Nextcloud Primary Storage
+resource "aws_s3_bucket" "nextcloud_primary" {
+   bucket = "nextcloud.${var.domain}"
+}
+resource "aws_s3_bucket_public_access_block" "nextcloud_primary" {
+  bucket = aws_s3_bucket.nextcloud_primary.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
